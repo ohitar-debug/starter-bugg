582beeb95a230c639aa8359f6a00ba7ab2e4db95
salimM21 <salim.majide.officiel@gmail.com>
2025-08-15 16:01:03 +0100

    SALIM

--- forecast.py
+++ forecast.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import TimeSeriesSplit
 
 
 def make_train_test(df: pd.DataFrame):
