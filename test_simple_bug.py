3d0c1e25049eb94ff6b6f72b081dfe46c4bb39cc
ohitar-debug <ohitar@simplon.co>
2025-08-15 01:27:17 +0100

    Initial commit

---
+++ test_simple_bug.py
@@ -0,0 +1,9 @@
+from simple_bug import divide
+import pytest
+
+def test_divide_ok():
+    assert divide(10, 2) == 5
+
+def test_divide_zero():
+    with pytest.raises(ValueError):
+        divide(5, 0)
