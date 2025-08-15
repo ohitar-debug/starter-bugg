9241a77418d72def1cc340f6034d6a37b6b33c3f
ohitar-debug <ohitar@simplon.co>
2025-08-15 15:05:20 +0100

    omar

--- simple_bug.py
+++ simple_bug.py
@@ -1,4 +1,2 @@
 def divide(a, b):
-    if b == 0:
-        raise ValueError("Division par zero interdite")
     return a/b
--- test_simple_bug.py
+++ test_simple_bug.py
@@ -2,8 +2,6 @@ from simple_bug import divide
 import pytest
 
 
-def test_divide_ok():
-    assert divide(10, 5) == 2
 
 def test_divide_zero():
     with pytest.raises(ValueError):
