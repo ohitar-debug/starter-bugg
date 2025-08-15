48fb11bf19e047ae76a63621df345147d04410ba
fidelis <yonlifidelis2@gmail.com>
2025-08-15 16:28:14 +0100

    bug fixé solution 2

--- forecast.py
+++ forecast.py
@@ -14,34 +14,34 @@ from sklearn.model_selection import TimeSeriesSplit
 
 # SOLUTION 1: séparation temporelle
 
-def make_train_test(df: pd.DataFrame, test_size: float = 0.2):
-    """
-    Sépare les données sans les mélanger aléatoirement pour respecter l’ordre temporel.
-    """
-    df_sorted = df.sort_values("date")
-    split_index = int(len(df_sorted) * (1 - test_size))
-    train = df_sorted.iloc[:split_index]
-    test = df_sorted.iloc[split_index:]
-    return train, test
+# def make_train_test(df: pd.DataFrame, test_size: float = 0.2):
+#     """
+#     Sépare les données sans les mélanger aléatoirement pour respecter l’ordre temporel.
+#     """
+#     df_sorted = df.sort_values("date")
+#     split_index = int(len(df_sorted) * (1 - test_size))
+#     train = df_sorted.iloc[:split_index]
+#     test = df_sorted.iloc[split_index:]
+#     return train, test
 
 # SOLUTION 2 avec les quantiles
 
-# def make_train_test(df: pd.DataFrame, test_size: float = 0.2):
-#     """
-#     Sépare les données temporellement en utilisant le quantile de la colonne 'Date'
-#     pour éviter le mélange aléatoire et respecter l’ordre temporel.
-#     """
-#     # S'assurer que la colonne Date est au bon format
-#     df = df.copy()
-#     df["Date"] = pd.to_datetime(df["date"])
+def make_train_test(df: pd.DataFrame, test_size: float = 0.2):
+    """
+    Sépare les données temporellement en utilisant le quantile de la colonne 'Date'
+    pour éviter le mélange aléatoire et respecter l’ordre temporel.
+    """
+    # S'assurer que la colonne Date est au bon format
+    df = df.copy()
+    df["Date"] = pd.to_datetime(df["date"])
 
-#     # Calcul de la date seuil à partir du quantile
-#     split_date = df["Date"].quantile(1 - test_size)
+    # Calcul de la date seuil à partir du quantile
+    split_date = df["Date"].quantile(1 - test_size)
 
-#     # Split chronologique
-#     train = df[df["Date"] < split_date]
-#     test = df[df["Date"] >= split_date]
-#     return train, test
+    # Split chronologique
+    train = df[df["Date"] < split_date]
+    test = df[df["Date"] >= split_date]
+    return train, test
 
 
 
