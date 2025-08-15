f24de89cb4109ba33027e9f7acbe6c6eb026b316
ohitar-debug <ohitar@simplon.co>
2025-08-15 15:13:19 +0100

    omar

--- test_forecast.py
+++ test_forecast.py
@@ -1,15 +1,15 @@
-# import pandas as pd
-# from forecast import make_train_test
+import pandas as pd
+from forecast import make_train_test
 
-# def test_no_data_leakage_on_dates():
-#     """
-#     Le test est VERT si TOUTES les dates de test sont postérieures à TOUTES les dates de train.
-#     Avec le split aléatoire, ce test DOIT échouer (ROUGE).
-#     """
-#     # Génère un DF simple directement depuis data.csv pour coller au vrai schéma
-#     df = pd.read_csv("data.csv", parse_dates=["date"])
+def test_no_data_leakage_on_dates():
+    """
+    Le test est VERT si TOUTES les dates de test sont postérieures à TOUTES les dates de train.
+    Avec le split aléatoire, ce test DOIT échouer (ROUGE).
+    """
+    # Génère un DF simple directement depuis data.csv pour coller au vrai schéma
+    df = pd.read_csv("data.csv", parse_dates=["date"])
 
-#     train, test = make_train_test(df)
+    train, test = make_train_test(df)
 
-#     assert test["date"].min() > train["date"].max(), \
-#         "Fuite de données détectée : le split n'est pas temporel (dates train/test se chevauchent)."
+    assert test["date"].min() > train["date"].max(), \
+        "m"
--- test_simple_bug.py
+++
@@ -1,8 +0,0 @@
-from simple_bug import divide
-import pytest
-
-
-
-def test_divide_zero():
-    with pytest.raises(ValueError):
-        divide(5, 0)
